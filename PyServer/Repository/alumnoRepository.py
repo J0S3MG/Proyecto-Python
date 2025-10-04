f1b0ca82112e17f59c9806b483b646d3cf798209
J0S3MG <m0n0.gj81@gmail.com>
2025-10-04 14:44:51 -0300

    Acomodado

---
+++ PyServer/Repository/alumnoRepository.py
@@ -0,0 +1,52 @@
+from typing import List, Optional # El Optional es para poder decir que es lo que devuelve un metodo.
+from sqlalchemy.orm import Session # Esto nos permitira iniciar la sesion con la BD.
+from Models.alumno import Alumno  # Traemos al obj Alumno.
+from Models.alumnoDB import AlumnoDB  # Traemos nuestro ORM.
+
+# En este archivo definimos la clase que se encargara de la sincronizacion con la db.
+class AlumnoRepository:
+    def __init__(self, db: Session): # Ahora en vez de inicializar una lista le pasamos una sesion.
+        self.db = db
+
+    # Obtener todos los alumnos
+    def get_alumno_all(self) -> List[Alumno]:
+        alumnos_db = self.db.query(AlumnoDB).all() # Aca hacemos la consulta a la tabla en la BD (SELECT * FROM Alumno).
+        return [Alumno.from_orm(alu) for alu in alumnos_db] # Aca transformamos cada fila en un Obj Alumno.
+
+    # Obtener alumno por legajo
+    def get_by_legajo(self, legajo: int) -> Optional[Alumno]: # Le decimos que devuelve un Alumno o None.
+        alu_db = self.db.query(AlumnoDB).filter(AlumnoDB.legajo == legajo).first() # Buscamos al alumno en la base de datos.
+        return Alumno.from_orm(alu_db) if alu_db else None # Si lo encuentra lo devuelve sino devuelve None.
+
+    # Crear alumno
+    def create_alumno(self, alu: Alumno) -> Alumno:
+        alu_db = AlumnoDB( # Construimos el obj AlumnoDB a partir de los datos de alu.
+            nombre=alu.nombre,
+            legajo=alu.legajo,
+            nota=alu.nota
+        )
+        self.db.add(alu_db) # Marca ese objeto para ser insertado en la base de datos, aun no ejecuta el INSERT.
+        self.db.commit() # Confirmamos los cambios y ejecuta el INSERT.
+        self.db.refresh(alu_db)  # Refrescamos la fila (alu_db) para autogenerar el Id.
+        return Alumno.from_orm(alu_db) # Reconvertimos el alu_db a un Alumno.
+
+    # Actualizar alumno: El proceso es parecido al del Create solo que aca buscamos al alumno primero.
+    def update_alumno(self, alu_upd: Alumno, alu_id: int) -> Optional[Alumno]:
+        alu_db = self.db.query(AlumnoDB).filter(AlumnoDB.id == alu_id).first()
+        if alu_db:
+            alu_db.nombre = alu_upd.nombre
+            alu_db.legajo = alu_upd.legajo
+            alu_db.nota = alu_upd.nota
+            self.db.commit()
+            self.db.refresh(alu_db)
+            return Alumno.from_orm(alu_db)
+        return None
+
+    # Eliminar alumno
+    def delete_alumno(self, legajo: int) -> Optional[dict]: # En este caso devolvemos un msj.
+        alu_db = self.db.query(AlumnoDB).filter(AlumnoDB.legajo == legajo).first() # Buscamos al Alumno.
+        if alu_db: # Si lo encuentra lo borra y confirma la accion con commit().
+            self.db.delete(alu_db)
+            self.db.commit()
+            return {"detalle": "Alumno borrado correctamente"}
+        return None
--- PyServer/Services/alumnoService.py
+++ PyServer/Services/alumnoService.py
@@ -1,7 +1,7 @@
 from typing import List, Optional # El Optional es para poder decir que es lo que devuelve un metodo.
 from sqlalchemy.orm import Session # Esto nos permitira iniciar la sesion con la BD.
 from Models.alumno import Alumno  # Traemos al obj Alumno.
-from Repository.alumnoDB import AlumnoDB  # Traemos nuestro ORM.
+from Repository.alumnoRepository import AlumnoRepository
 
 # En este archivo definimos la clase que se encargara de la logica del negocio (CRUD).
 class AlumnoService:
@@ -10,45 +10,26 @@ class AlumnoService:
 
     # Obtener todos los alumnos
     def get_alumno_all(self) -> List[Alumno]:
-        alumnos_db = self.db.query(AlumnoDB).all() # Aca hacemos la consulta a la tabla en la BD (SELECT * FROM Alumno).
-        return [Alumno.from_orm(alu) for alu in alumnos_db] # Aca transformamos cada fila en un Obj Alumno.
+        alu_db = AlumnoRepository(self.db)
+        return alu_db.get_alumno_all() 
 
     # Obtener alumno por legajo
     def get_by_legajo(self, legajo: int) -> Optional[Alumno]: # Le decimos que devuelve un Alumno o None.
-        alu_db = self.db.query(AlumnoDB).filter(AlumnoDB.legajo == legajo).first() # Buscamos al alumno en la base de datos.
-        return Alumno.from_orm(alu_db) if alu_db else None # Si lo encuentra lo devuelve sino devuelve None.
+        alu_db = AlumnoRepository(self.db)
+        return alu_db.get_by_legajo(legajo)
 
     # Crear alumno
     def create_alumno(self, alu: Alumno) -> Alumno:
-        alu_db = AlumnoDB( # Construimos el obj AlumnoDB a partir de los datos de alu.
-            nombre=alu.nombre,
-            legajo=alu.legajo,
-            nota=alu.nota
-        )
-        self.db.add(alu_db) # Marca ese objeto para ser insertado en la base de datos, aun no ejecuta el INSERT.
-        self.db.commit() # Confirmamos los cambios y ejecuta el INSERT.
-        self.db.refresh(alu_db)  # Refrescamos la fila (alu_db) para autogenerar el Id.
-        return Alumno.from_orm(alu_db) # Reconvertimos el alu_db a un Alumno.
+        alu_db = AlumnoRepository(self.db)
+        return alu_db.create_alumno(alu)
 
     # Actualizar alumno: El proceso es parecido al del Create solo que aca buscamos al alumno primero.
     def update_alumno(self, alu_upd: Alumno, alu_id: int) -> Optional[Alumno]:
-        alu_db = self.db.query(AlumnoDB).filter(AlumnoDB.id == alu_id).first()
-        if alu_db:
-            alu_db.nombre = alu_upd.nombre
-            alu_db.legajo = alu_upd.legajo
-            alu_db.nota = alu_upd.nota
-            self.db.commit()
-            self.db.refresh(alu_db)
-            return Alumno.from_orm(alu_db)
-        return None
+        alu_db = AlumnoRepository(self.db)
+        return alu_db.update_alumno(alu_upd,alu_id)
 
     # Eliminar alumno
     def delete_alumno(self, legajo: int) -> Optional[dict]: # En este caso devolvemos un msj.
-        alu_db = self.db.query(AlumnoDB).filter(AlumnoDB.legajo == legajo).first() # Buscamos al Alumno.
-        if alu_db: # Si lo encuentra lo borra y confirma la accion con commit().
-            self.db.delete(alu_db)
-            self.db.commit()
-            return {"detalle": "Alumno borrado correctamente"}
-        return None
-
+        alu_db = AlumnoRepository(self.db)
+        return alu_db.delete_alumno(legajo)
 
